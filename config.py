2a9a0254ef62e73b6632d0bfd97ed4fa943ecd18
fegloff <fegloff@gmail.com>
2023-10-13 16:39:07 -0500

    fix dependencies issues (work in progress)

--- config.py
+++ config.py
@@ -8,7 +8,8 @@ class Config(object):
     DEBUG = os.environ.get('DEBUG') if os.environ.get('DEBUG') else True
     TESTING = os.getenv('TESTING') if os.environ.get('DEBUG') else True
     SECRET_KEY = generate_new_secret_key()
-    CHROMA_SERVER_PATH = os.getenv('CHROMA_SERVER_PATH') if os.getenv('CHROMA_SERVER_PATH') else "/app/data/chroma"
+    CHROMADB_SERVER_URL = os.getenv('CHROMADB_SERVER_URL')
+    SQLITE_PATH = os.getenv('SQLITE_PATH') if os.getenv('SQLITE_PATH') else "/app/data/chroma"
     OPENAI_MODEL = os.getenv('OPENAI_MODEL') if os.getenv('OPENAI_MODEL') else "gpt-3.5-turbo"
     OPENAI_MAX_TOKENS = os.getenv('OPENAI_MAX_TOKENS') if os.getenv('OPENAI_MAX_TOKENS') else 600
     WEB_CRAWLER_HTTP = os.environ.get('WEB_CRAWLER_HTTP')
--- storages/__init__.py
+++ storages/__init__.py
@@ -2,12 +2,11 @@ from .chromadb_storage import ChromaStorage
 from chromadb.config import Settings
 from config import config
 
+host = config.CHROMADB_SERVER_URL
 
 client_settings = Settings(
-    is_persistent= True,
+      is_persistent= True,
     allow_reset=True
 )
-
-path = config.CHROMA_SERVER_PATH
-
-chromadb = ChromaStorage(path, client_settings)
+path = config.SQLITE_PATH
+chromadb = ChromaStorage(host, path, client_settings)
