06efc36ef42d43b86389fce5db741e77608e7359
fegloff <fegloff@gmail.com>
2024-10-24 16:38:18 -0500

    add xai resource

--- apis/__init__.py
+++ apis/__init__.py
@@ -1,6 +1,7 @@
 from flask_restx import Api
 from .vertex_resource import api as vertex_namespace, VertexGeminiCompletionRes
 from .llms_resource import api as llms_namespace
+from .xai_resource import api as xai_namespace
 from .collections import api as collections_namespace
 from .openai_resource import api as openai_namespace
 from .anthropic import api as anthropic_namespace, AnthropicCompletionRes
@@ -19,3 +20,4 @@ api.add_namespace(collections_namespace)
 api.add_namespace(openai_namespace)
 api.add_namespace(anthropic_namespace)
 api.add_namespace(lumaai_namespace)
+api.add_namespace(xai_namespace)
